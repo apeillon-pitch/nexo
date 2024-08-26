2bbff52342378372e957a22f7305def06f1595d0
apeillon-pitch <apeillon@lepitch.fr>
2024-08-26 10:18:59 +0200

    Update flex_team_style_one.blade.php

--- web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/flex_team_style_one.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/partials/flexible-sections/flex_team_style_one.blade.php
@@ -34,61 +34,66 @@
       @endforeach
     </div>
   </div>
-  @if($item['team'])
-    @foreach($item['team'] as $el)
-      @php $member = getMemberById($el); @endphp
-      <div class="modal fade" id="teamModal-{{ $loop->iteration }}" tabindex="-1"
-           aria-labelledby="teamModalLabel-{{ $loop->iteration }}"
-           aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-lg">
-          <div class="modal-content">
-            <div class="modal-header">
-              <div class="d-flex flex-row justify-content-between w-100 wp-content">
-                <div class="d-flex flex-column flex-lg-row">
-                  @if($member['picture'])
-                    <div class="thumbnail me-4"
-                         style="background-image: url({{ $member['picture'] }})"></div>
-                  @endif
-                  <div class="d-flex flex-column wp-header">
-                    <h3 class="modal-title mt-2" id="exampleModalLabel">{!! $member['title'] !!}</h3>
-                    @if($member['job'])
-                      <div class="d-flex flex-row">
-                        <img src="@asset('../../images/briefcase.svg')" alt="job" class="me-2">
-                        <span>{!! $member['job'] !!}</span>
-                      </div>
+  @php $i = 0 @endphp
+  @foreach($section['item_repeater'] as $item)
+    @if($item['team'])
+      @php $i = 0 @endphp
+      @foreach($item['team'] as $el)
+        @php $member = getMemberById($el); @endphp
+        <div class="modal fade" id="teamModal-{{ $i }}-{{ $loop->iteration }}" tabindex="-1"
+             aria-labelledby="teamModalLabel-{{ $i }}-{{ $loop->iteration }}"
+             aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+              <div class="modal-header">
+                <div class="d-flex flex-row justify-content-between w-100 wp-content">
+                  <div class="d-flex flex-column flex-lg-row">
+                    @if($member['picture'])
+                      <div class="thumbnail me-4"
+                           style="background-image: url({{ $member['picture'] }})"></div>
                     @endif
-                    @if($member['phone'])
-                      <div class="d-flex flex-row">
-                        <img src="@asset('../../images/Telephone.svg')" alt="tel" class="me-2">
-                        <span>{!! $member['phone'] !!}</span>
-                      </div>
-                    @endif
-                    @if($member['email'])
-                      <div class="d-flex flex-row">
-                        <img src="@asset('../../images/Email.svg')" alt="email" class="me-2">
-                        <span>{!! $member['email'] !!}</span>
-                      </div>
-                    @endif
+                    <div class="d-flex flex-column wp-header">
+                      <h3 class="modal-title mt-2" id="exampleModalLabel">{!! $member['title'] !!}</h3>
+                      @if($member['job'])
+                        <div class="d-flex flex-row">
+                          <img src="@asset('../../images/briefcase.svg')" alt="job" class="me-2">
+                          <span>{!! $member['job'] !!}</span>
+                        </div>
+                      @endif
+                      @if($member['phone'])
+                        <div class="d-flex flex-row">
+                          <img src="@asset('../../images/Telephone.svg')" alt="tel" class="me-2">
+                          <span>{!! $member['phone'] !!}</span>
+                        </div>
+                      @endif
+                      @if($member['email'])
+                        <div class="d-flex flex-row">
+                          <img src="@asset('../../images/Email.svg')" alt="email" class="me-2">
+                          <span>{!! $member['email'] !!}</span>
+                        </div>
+                      @endif
+                    </div>
                   </div>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-            </div>
-            <div class="modal-body">
-              @if($member['bio'])
-                <h3 class="section-title mb-4">Biographie</h3>
-                {!! $member['bio']!!}
-              @endif
-              @if($member['linkedin'])
-                <a href="{!! $member['linkedin']['url'] !!}" class="btn btn-primary"
-                   target="{{ $member['linkedin']['target'] }}" aria-label="{!! $member['linkedin']['title'] !!}">
-                  Se connecter <i class="ms-1 fa-brands fa-linkedin-in"></i>
-                </a>
-              @endif
+              <div class="modal-body">
+                @if($member['bio'])
+                  <h3 class="section-title mb-4">Biographie</h3>
+                  {!! $member['bio']!!}
+                @endif
+                @if($member['linkedin'])
+                  <a href="{!! $member['linkedin']['url'] !!}" class="btn btn-primary"
+                     target="{{ $member['linkedin']['target'] }}" aria-label="{!! $member['linkedin']['title'] !!}">
+                    Se connecter <i class="ms-1 fa-brands fa-linkedin-in"></i>
+                  </a>
+                @endif
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    @endforeach
-  @endif
+      @endforeach
+    @endif
+    @php $i++@endphp
+  @endforeach
 </div>

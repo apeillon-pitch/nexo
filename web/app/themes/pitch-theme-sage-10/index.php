61f2e81aef19f719bbee7e855686d111511ce9d2
apeillon-pitch <apeillon@lepitch.fr>
2024-08-26 09:51:14 +0200

    Update index.php

--- web/app/themes/pitch-theme-sage-10/index.php
+++ web/app/themes/pitch-theme-sage-10/index.php
@@ -9,6 +9,16 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/0b51096d56.js" defer crossorigin="anonymous"></script>
+    <!-- Matomo Tag Manager -->
+    <script>
+      var _mtm = window._mtm = window._mtm || [];
+      _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+      (function() {
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src='https://cdn.matomo.cloud/nortia.matomo.cloud/container_YLn5xhXm.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <!-- End Matomo Tag Manager -->
     <?php wp_head(); ?>
   </head>
 

38dc78d5ada9922983f72b4444e0bca31da0a8ca
apeillon-pitch <apeillon@lepitch.fr>
2024-08-26 10:26:36 +0200

    update

--- web/app/themes/pitch-theme-sage-10/resources/views/sections/footer.blade.php
+++ web/app/themes/pitch-theme-sage-10/resources/views/sections/footer.blade.php
@@ -4,7 +4,8 @@
       <div class="col-12 col-lg-3">
         @if($footer['data']['logo'])
           <a href="{{ home_url() }}" aria-label="Accueil">
-            <img src="{{ $footer['data']['logo']['url'] }}" width="110" height="80" class="mb-4" alt="{!!  $footer['data']['logo']['alt'] !!}">
+            <img src="{{ $footer['data']['logo']['url'] }}" width="110" height="80" class="mb-4"
+                 alt="{!!  $footer['data']['logo']['alt'] !!}">
           </a>
         @endif
         @if($footer['data']['text'])
@@ -31,24 +32,32 @@
           </div>
         @endif
       </div>
-      <div class="col-12 col-lg-3">
-        <div class="d-flex flex-column wp-cta">
-          @foreach($footer['data']['cta_repeater'] as $item)
-            <a href="{{ $item['link']['url'] }}" class="btn btn-{{ $item['class'] }}"
+      @if($footer['data']['cta_repeater'])
+        <div class="col-12 col-lg-3">
+          <div class="d-flex flex-column wp-cta">
+            @foreach($footer['data']['cta_repeater'] as $item)
+              <a href="{{ $item['link']['url'] }}" class="btn btn-{{ $item['class'] }}"
+                 aria-label="{!! $item['link']['title'] !!}" target="{{ $item['link']['target'] }}">
+                {!! $item['link']['title'] !!}
+              </a>
+            @endforeach
+          </div>
+        </div>
+      @endif
+    </div>
+    @if($footer['data']['link_repeater'])
+      <div class="d-flex flex-column flex-lg-row justify-content-between wp-legal">
+        <span>Copyright © 2024 Nexo Capital - Tous droits réservés.</span>
+        <div class="d-flex flex-row wp-links mt-3 mt-lg-0">
+          @foreach($footer['data']['link_repeater'] as $item)
+            <a href="{{ $item['link']['url'] }}"
                aria-label="{!! $item['link']['title'] !!}" target="{{ $item['link']['target'] }}">
               {!! $item['link']['title'] !!}
             </a>
           @endforeach
         </div>
       </div>
-    </div>
-    <div class="d-flex flex-column flex-lg-row justify-content-between wp-legal">
-      <span>Copyright © 2024 Nexo Capital - Tous droits réservés.</span>
-      <div class="d-flex flex-row wp-links mt-3 mt-lg-0">
-        <a href="#" aria-label="Mentions légales">Mentions légales</a>
-        <a href="#" aria-label="Données personnelles">Données personnelles</a>
-      </div>
-    </div>
+    @endif
   </div>
 </footer>
 @include('components.mobile-menu')

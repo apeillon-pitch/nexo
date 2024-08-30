215984b43c4be903964c0d6f079a63b714cc5c95
apeillon-pitch <apeillon@lepitch.fr>
2024-08-30 10:00:30 +0200

    update

--- web/app/themes/pitch-theme-sage-10/resources/scripts/app.js
+++ web/app/themes/pitch-theme-sage-10/resources/scripts/app.js
@@ -39,10 +39,20 @@ const main = async (err) => {
           autoplay: false,
           responsive: [
             {
-              breakpoint: 1024,
+              breakpoint: 1200,
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
+                infinite: false,
+                arrows: true,
+                dots: false,
+              },
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
                 infinite: false,
                 arrows: true,
                 dots: false,
